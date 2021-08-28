ebfdb761a1d4c26df11d866705f501f51db77c91
Sam F <samfondacaro2006@gmail.com>
2021-08-28 17:16:24 -0400

    Added bar_length parameter

--- src/main.py
+++ src/main.py
@@ -1,27 +1,36 @@
+from time import sleep
+
 class ProgressBar:
-	def __init__(self, start=0, total=100, char="="):
+	def __init__(self, start=0, total=100, length=20, char="=", increment_delay=1):
 		self.current = start
 		self.total = total
 		self.char = char
+		self.length = length
+		self.increment_delay = increment_delay
 		self.percent = float(self.current) * 100 / self.total
 		self._generate_bar()
 
 	def increment(self, val=1):
 		self.current += val
 		self.percent = float(self.current) * 100 / self.total
-		if self.percent < 100:
+		if self.percent <= 100:
 			self._generate_bar()
 			print('\r', self.bar, end='')
+			sleep(self.increment_delay)
 		else:
 			raise ValueError("Increment value greater than total progress bar value")
 
 	def show(self):
-		print('\r', self.bar, end='')
+		print(self.bar, end='')
 
 	def _generate_bar(self):
-		self.bar = f"[{self.char * self.current}  {' ' * (self.total - self.current)}>] {self.percent}%"
+		self.num_chars = int((self.percent / 100) * self.length)
+		self.bar = f"[{self.char * self.num_chars}{' ' * (self.length - self.num_chars)}>] {self.percent}%"
 
 if __name__ == '__main__':
-	bar = ProgressBar()
+	bar = ProgressBar(length=40, start=10, total=120)
 	bar.show()
+	bar.increment(10)
+	bar.increment(10)
+	bar.increment(80)
 	bar.increment(10)
